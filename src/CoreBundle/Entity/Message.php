77431dcb01fa35f383e173149d9b2b7ec99ff030
Pacific <mupacif@gmail.com>
2017-11-07 16:27:04 +0100

    added repository

--- src/CoreBundle/Entity/Message.php
+++ src/CoreBundle/Entity/Message.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mupac_000
- * Date: 06-11-17
- * Time: 15:13
- */
-
 namespace CoreBundle\Entity;
 
 
---
+++ src/CoreBundle/Repository/DemandeRepository.php
@@ -0,0 +1,43 @@
+<?php
+namespace CoreBundle\Repository;
+use Doctrine\DBAL\Connection;
+use CoreBundle\Entity\Demande;
+
+
+class DemandeRepository
+{
+    private $db;
+
+    public function __construct(Connection $db) {
+        $this->db = $db;
+    }
+
+    public static $TABLENAME = "description";
+    public static $DESCRIPTION = "description";
+    public static $ID = "_ID";
+    public static $POIDS = "poids";
+    public static $DATE = "date";
+    public static $USERID = "idUser";
+
+    public function find($id) {
+        $sql = "select * from ".DemandeRepository::$TABLENAME." where ".DemandeRepository::$ID."=?";
+        $row = $this->db->fetchAssoc($sql, array($id));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new \Exception("No user matching id " . $id);
+    }
+
+    protected function buildDomainObject(array $row) {
+        $demande = new Demande();
+        $demande->setId($row[DemandeRepository::$ID]);
+        $demande->setDescription($row[DemandeRepository::$DESCRIPTION]);
+        $demande->setPoids($row[DemandeRepository::$POIDS]);
+        $demande->setDate($row[DemandeRepository::$DATE]);
+        $demande->setUserId($row[DemandeRepository::$USERID]);
+
+
+        return $demande;
+    }
+}
